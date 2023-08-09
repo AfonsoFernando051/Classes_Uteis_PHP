562a23444a285e78cab5c599cfa6cc076a0d3ddd
Fernando Afonso <102692428+AfonsoFernando051@users.noreply.github.com>
2023-08-09 14:13:12 -0300

    Classe melhorada

--- ClassPaginacao.php
+++ ClassPaginacao.php
@@ -3,19 +3,13 @@
 class Paginacao{
 
     private static $total_registros;
-    private static $total_pagina;
+    private static $total_de_paginas_navegacao;
   
-    static function getTotalRegistros(){
-       return self::$total_registros;
-    }
     static function setTotalRegistros($total_registros){
         self::$total_registros = $total_registros;
     }
-    static function getTotalPaginaNavegacao(){
-        return self::$total_pagina;
-    }
     static function setTotalPaginaNavegacao($total_registros, $registrosPorPagina){
-        self::$total_pagina = ceil($total_registros / $registrosPorPagina);
+        self::$total_de_paginas_navegacao = ceil($total_registros / $registrosPorPagina); // O total de páginas é calculado de acordo com o quociente do total de registros com a qtd de registros por página. 
     }
     
      /**
@@ -26,24 +20,22 @@ class Paginacao{
     * @param mixed $query a query principal sem o order by para contagem;
     * @param mixed $sql_orderby a query principal com order by para retornar função OFFSET;
     * @param mixed $params os parâmetros da query principal;
-    * @param mixed $url a $url;
-    * @param mixed $filtros e os filtros que montam a url;
-    * É setado com funções auxiliares o total de linhas retornadas pela query no atributo privado $total_registros
-    * e a quantidade de páginas disponíveis para navegação no atributo $total_pagina.
-    * Caso o atributo $total_pagina tenha valor = 1, não existirá o <nav> para vários registros, do contrário, é montada com a função gerarPaginacao(), um menu de navegação.
+
     * A função paginar retorna um array com valores que possuem:
     * A query principal com a função de offset - 'concat_query';
     * O HTML com os hrefs para paginação - 'paginacao';
     * O total de registros para impressão na tela = 'total_de_registros';
     * O offset que caracterizará a orderm das linhas - 'offset'.
     */
-    static function paginar($paginaAtual, $registrosPorPagina, $query, $sql_orderby, $params, $url, $filtros){
+    static function paginar($paginaAtual, $registrosPorPagina, $query, $sql_orderby, $params){
+        $url = $_SERVER['SCRIPT_NAME']; //Nome do arquivo que vêm na URL;
+        $filtros = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY); //Parâmetros que vêm na URL;
+    
+        self::$total_registros = self::queryTotaldeLinhas($query, $params); // O atributo $total_registros recebe o total de registros da query;
+        self::setTotalPaginaNavegacao(self::$total_registros, $registrosPorPagina); //O atributo $total_de_paginas_navegacao recebe a quantidade de páginas disponíveis para navegação.
+        $offset = ($paginaAtual - 1) * $registrosPorPagina; // Offset são as linhas dos registros nas diferentes páginas.
 
-        self::$total_registros = self::queryTotaldeLinhas($query, $params);
-        self::setTotalPaginaNavegacao(self::$total_registros, $registrosPorPagina);
-        $offset = ($paginaAtual - 1) * $registrosPorPagina;
-
-        $paginacao = (self::$total_pagina > 1)? self::gerarPaginacao(self::getTotalPaginaNavegacao(), $paginaAtual, $url, $filtros) : NULL;
+        $paginacao = (self::$total_de_paginas_navegacao > 1)? self::gerarPaginacao(self::$total_de_paginas_navegacao, $paginaAtual, $url, $filtros) : NULL; //Função que retorna o HTML da paginação caso haja mais de uma página.
 
         return array(
             'concat_query' =>  "$sql_orderby OFFSET $offset ROWS FETCH NEXT $registrosPorPagina ROWS ONLY",
@@ -72,12 +64,14 @@ class Paginacao{
     
     }
 
-    /*Função que retorna a o menu de paginção com os links corretos.*/
-    private static function gerarPaginacao($total_pagina, $pagina, $url, $filtros){
+    /*Função que retorna a o menu de paginção com os links corretos.
+    O CSS utiliza algumas classes do Bootstrap 4.3 em algumas etapas.
+    */
+    private static function gerarPaginacao($total_de_paginas_navegacao, $pagina, $url, $filtros){
 
         $maxPagesToShow = 5;
         $firstPageToShow = max(1, $pagina - floor($maxPagesToShow / 2));
-        $lastPageToShow = min($total_pagina, $firstPageToShow + $maxPagesToShow - 1);
+        $lastPageToShow = min($total_de_paginas_navegacao, $firstPageToShow + $maxPagesToShow - 1);
         $firstPageToShow = max(1, $lastPageToShow - $maxPagesToShow + 1);
         $pagination = "
             <style>
@@ -105,8 +99,8 @@ class Paginacao{
                                 <a class='page-link' href='{$url}?{$filtros}&pagina={$page}'>{$page}</a>
                             </li>";
         }
-        if ($page < $total_pagina) {
-            $proxima = ($total_pagina > $pagina)? "{$url}?{$filtros}&pagina=".($pagina+1): "#";
+        if ($page < $total_de_paginas_navegacao) {
+            $proxima = ($total_de_paginas_navegacao > $pagina)? "{$url}?{$filtros}&pagina=".($pagina+1): "#";
             $pagination .= "<li class='page-item {$active}'>
                                 <a class='page-link' href='{$proxima}'>Próxima</a>
                             </li>";
